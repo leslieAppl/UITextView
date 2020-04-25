0fdc19b00ef85252752450a9ba913cced7d2acbf
Leslie <leslie.appl@gmail.com>
2020-04-25 18:13:42 +0800

    updated..

---
+++ README.md
@@ -0,0 +1,10 @@
+# UITextView
+
+## textView.textStorage
+
+## UITextViewDelegate
+
+## Find and return the ranges of all the occurrences of a given string in Swift
+
+
+
--- UITextView/ViewController.swift
+++ UITextView/ViewController.swift
@@ -10,11 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var message: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        message.delegate = self
     }
 
+    @IBAction func selection(_ sender: UIButton) {
+        let attributedText = message.textStorage
+        attributedText.addAttribute(.foregroundColor, value: UIColor.red, range: message.selectedRange)
+        message.selectedTextRange = nil
+    }
+    
+}
+
+extension ViewController: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let text = textView.text
+        
+        //Finds and returns the range of the [first occurrence] of a given string
+//        if let range = text?.range(of: "John", options: .caseInsensitive) {
+//            let attributedText = textView.textStorage
+//            attributedText.addAttribute(.foregroundColor, value: UIColor.green, range: NSRange(range,in: text!))
+//        }
+        
+        //Find and return the ranges of [all the occurrences] of a given string in Swift
+        if let ranges = text?.rangesOf(string: "John", options: .caseInsensitive) {
+            for range in ranges {
+                //because textStorage: NSTextStorage class is the reference type
+                //As assigned the textStorage property to attributedText variable
+                //means that both of attributedText and textStorage variables share the same object
+                //to change any of one is the same thing.
+                //That's the different between class and structure
+                let attributedText = textView.textStorage
+                attributedText.addAttribute(.foregroundColor, value: UIColor.green, range: NSRange(range, in: text!))
+            }
+        }
+    }
+    
+}
+
+extension String {
+    
+    func rangesOf(string: String, options: NSString.CompareOptions) -> [Range<String.Index>] {
+        
+        var ranges: [Range<String.Index>] = []
+        var searchStartIndex = self.startIndex
+        
+        while searchStartIndex < self.endIndex,
+            let range = self.range(of: string, options: options, range: searchStartIndex..<self.endIndex){
+                
+                ranges.append(range)
+                
+                searchStartIndex = range.upperBound
+        }
+        
+        return ranges
+    }
+    
 
 }
+
+
 
